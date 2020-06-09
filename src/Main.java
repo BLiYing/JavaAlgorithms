3ec7d9da3d1055147cf84430465189aa1eb361f4
BLiYing <872904178@qq.com>
2020-06-09 14:27:07 +0800

    leetcode算法两题

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,78 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] nums = {1,2,3,5,6,7,3,4,3,9,10};
+
+//        System.out.println("重复的数字：" + findDuplicate(nums));
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int maxArea = maxArea(height);
+        System.out.println("maxArea = [" + maxArea + "]");
     }
+
+    /**
+     * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/find-the-duplicate-number
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * 不能更改原数组（假设数组是只读的）。
+     * 只能使用额外的 O(1) 的空间。
+     * 时间复杂度小于 O(n2) 。
+     * 数组中只有一个重复的数字，但它可能不止重复出现一次。
+     * @param nums
+     * @return
+     */
+    public static int findDuplicate(int[] nums) {
+        /**
+         快慢指针思想, fast 和 slow 是指针, nums[slow] 表示取指针对应的元素
+         注意 nums 数组中的数字都是在 1 到 n 之间的(在数组中进行游走不会越界),
+         因为有重复数字的出现, 所以这个游走必然是成环的, 环的入口就是重复的元素,
+         即按照寻找链表环入口的思路来做
+         **/
+        int fast = 0, slow = 0;
+        while(true) {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            System.out.println(nums[slow]);
+            if(slow == fast) {
+                fast = 0;
+                while(nums[slow] != nums[fast]) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return nums[slow];
+            }
+        }
+    }
+
+    /**
+     * 盛最多水的容器
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     *
+     * 说明：你不能倾斜容器，且 n 的值至少为 2。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/container-with-most-water
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param height
+     * @return
+     */
+    public static int maxArea(int[] height) {
+        int size = height.length;
+        int maxArea = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int area = Math.min(height[i],height[j]) * (j - i);
+                if(area >= maxArea){
+                    maxArea = area;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+
+
 }
